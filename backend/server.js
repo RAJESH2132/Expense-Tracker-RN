7a97a3b7bd27ec1201fa670bf5dddcefe33c6e61
RAJESH2132 <rajeshnagurla@gmail.com>
2025-09-16 21:43:37 +0530

    implement create transactions

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import { sql } from "./config/db.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 5001;
 
 async function initDB() {
@@ -16,9 +18,9 @@ async function initDB() {
     amount DECIMAL(10,2) NOT NULL,
     category VARCHAR(255) NOT NULL,
     created_at DATE NOT NULL DEFAULT CURRENT_DATE
-    )`
+    )`;
 
-    console.log("database intialized")
+    console.log("database intialized");
   } catch (error) {
     console.log("Error initializing DB", error);
     process.exit(1);
@@ -29,8 +31,24 @@ app.get("/", (req, res) => {
   res.send("running");
 });
 
-initDB().then(()=>{
-  app.listen(PORT, ()=>{
-    console.log("Server is running on PORT:", PORT)
-  })
-})
+app.post("/api/transactions", async (req, res) => {
+  try {
+    const { title, amount, category, user_id } = req.body;
+    if (!title || !user_id || !category || amount === undefined) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const transaction =
+      await sql`INSERT INTO transactions(user_id,title,amount,category)
+    VALUES (${user_id},${title},${amount},${category}) RETURNING *`;
+  } catch (error) {
+    console.log("Error creating the transaction", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on PORT:", PORT);
+  });
+});
